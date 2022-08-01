f59bc382c2bdefa885cb325139504f1997bfa41a
Lokirovskii <719220502@qq.com>
2022-08-01 16:57:10 +0800

    team management

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
   },
   {
     path: '/about',
@@ -16,14 +16,19 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
   },
   {
     path: '/teammanagement',
     name: 'teammanagement',
-    component: () => import('../views/TeamManagement.vue')
-  }
-]
+    component: () => import('../views/TeamManagement.vue'),
+  },
+  // {
+  //   path: '/documenteditor',
+  //   name: 'documenteditor',
+  //   component: () => import('../views/DocumentEditor.vue'),
+  // },
+];
 
 const router = new VueRouter({
   mode: 'history',
--- src/views/TeamManagement.vue
+++ src/views/TeamManagement.vue
@@ -6,9 +6,13 @@
       <el-table-column prop="truename" label="真实姓名" min-width="20%"></el-table-column>
       <el-table-column prop="email" label="邮箱" min-width="30%"></el-table-column>
       <el-table-column min-width="30%">
-        <template>
-          <el-button type="primary" size="small">设为管理</el-button>
-          <el-button type="danger" size="small">移出团队</el-button>
+        <template scope="scope">
+          <el-button type="primary" size="small" @click="set_admin(scope.row.userid)"
+            >设为管理</el-button
+          >
+          <el-button type="danger" size="small" @click="remove_user(scope.row.userid)"
+            >移出团队</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
@@ -21,15 +25,27 @@
 
     data() {
       return {
+        i_am_admin: true,
         testdata: [
           {
             userid: 5,
             username: 'haha',
             truename: '哈哈',
             email: '11@qq.com',
+            isadmin: true,
           },
         ],
       };
+    },
+
+    methods: {
+      set_admin(userid) {
+        this.$message.success('成功设置' + userid + '为管理员');
+      },
+
+      remove_user(userid) {
+        this.$message.success('成功移出' + userid);
+      },
     },
   };
 </script>
