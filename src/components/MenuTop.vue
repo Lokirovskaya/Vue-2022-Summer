6a90bcf690c608fe15ca54bbcf5e8571799f1602
takune <1525124406@qq.com>
2022-08-02 20:30:22 +0800

    添加一个组件库,修改了项目主页中与后端的接口

--- src/components/MenuTop.vue
+++ src/components/MenuTop.vue
@@ -1,65 +1,65 @@
 <template>
-    <div id="top">
-      <el-col :span="12">
-        <div class="grid-content">
-          <el-menu :default-active="active_index" class="el-menu-demo" mode="horizontal" @select="SelectIndex">
+  <div id="top">
+    <el-col :span="12">
+      <div class="grid-content">
+        <el-menu :default-active="active_index" class="el-menu-demo" mode="horizontal" @select="SelectIndex">
 
-            <el-menu-item index="1">
-              导航1
-            </el-menu-item>
+          <el-menu-item index="1">
+            导航1
+          </el-menu-item>
 
-            <el-submenu index="2">
-              <template slot="title">导航2</template>
-              <router-link :to="{ path: 'sector', query: { name: 'discussion' } }">
-                <el-menu-item index="2-1"><i class="el-icon-position"></i>导航21</el-menu-item>
-              </router-link>
-              <router-link :to="{ path: 'sector', query: { name: 'recommendation' } }">
-                <el-menu-item index="2-2"><i class="el-icon-collection"></i>导航22</el-menu-item>
-              </router-link>
-              <router-link :to="{ path: 'sector', query: { name: 'exercise' } }">
-                <el-menu-item index="2-3"><i class="el-icon-medal"></i>导航23</el-menu-item>
-              </router-link>
-              <router-link :to="{ path: 'sector', query: { name: 'campus' } }">
-                <el-menu-item index="2-4"><i class="el-icon-present"></i>导航24</el-menu-item>
-              </router-link>
-              <router-link :to="{ path: 'sector', query: { name: 'resource' } }">
-                <el-menu-item index="2-5"><i class="el-icon-download"></i>导航25</el-menu-item>
-              </router-link>
-            </el-submenu>
-            <!--
+          <el-submenu index="2">
+            <template slot="title">导航2</template>
+            <router-link :to="{ path: 'sector', query: { name: 'discussion' } }">
+              <el-menu-item index="2-1"><i class="el-icon-position"></i>导航21</el-menu-item>
+            </router-link>
+            <router-link :to="{ path: 'sector', query: { name: 'recommendation' } }">
+              <el-menu-item index="2-2"><i class="el-icon-collection"></i>导航22</el-menu-item>
+            </router-link>
+            <router-link :to="{ path: 'sector', query: { name: 'exercise' } }">
+              <el-menu-item index="2-3"><i class="el-icon-medal"></i>导航23</el-menu-item>
+            </router-link>
+            <router-link :to="{ path: 'sector', query: { name: 'campus' } }">
+              <el-menu-item index="2-4"><i class="el-icon-present"></i>导航24</el-menu-item>
+            </router-link>
+            <router-link :to="{ path: 'sector', query: { name: 'resource' } }">
+              <el-menu-item index="2-5"><i class="el-icon-download"></i>导航25</el-menu-item>
+            </router-link>
+          </el-submenu>
+          <!--
             <el-menu-item index="3" v-show="this.$store.getters.is_admin">
               管理员界面
             </el-menu-item>
             -->
-          </el-menu>
+        </el-menu>
+      </div>
+    </el-col>
+    <el-col :span="12">
+      <div class="grid-content" style="text-align: right">
+        <div v-if="this.$store.state.login_state === 0">
+          <el-link :underline="false" style="position: relative;bottom:-19px" @click="goto_login">登录/注册
+          </el-link>
+          &nbsp;&nbsp;
         </div>
-      </el-col>
-      <el-col :span="12">
-        <div class="grid-content" style="text-align: right">
-          <div v-if="this.$store.state.login_state === 0">
-            <el-link :underline="false" style="position: relative;bottom:-19px" @click="goto_login">登录/注册
-            </el-link>
-            &nbsp;&nbsp;
-          </div>
-          <div v-else>
-            <el-dropdown @command="User_Command">
-              <div>
-                <img :src="'http://43.138.55.69' + this.$store.state.user_photo"
-                  style="height: 35px;width: 35px;position: relative;bottom: -14px; border-radius: 3px;">
-                <!--<el-avatar style="position: relative;bottom: -13.5px" shape="square" :size="35" v-bind:src="User.pho"></el-avatar>-->
-                <span style="position: relative;bottom: -3px">&nbsp;用户名&nbsp;&nbsp;</span>
-              </div>
-              <el-dropdown-menu slot="dropdown">
-                <router-link :to="{ path: '/personcenter', query: { user: this.$store.state.username } }">
-                  <el-dropdown-item command="a">个人中心</el-dropdown-item>
-                </router-link>
-                <el-dropdown-item command="b">退出登录</el-dropdown-item>
-              </el-dropdown-menu>
-            </el-dropdown>
-          </div>
+        <div v-else>
+          <el-dropdown @command="User_Command">
+            <div>
+              <img :src="'http://43.138.55.69' + this.$store.state.user_photo"
+                style="height: 35px;width: 35px;position: relative;bottom: -14px; border-radius: 3px;">
+              <!--<el-avatar style="position: relative;bottom: -13.5px" shape="square" :size="35" v-bind:src="User.pho"></el-avatar>-->
+              <span style="position: relative;bottom: -3px">&nbsp;{{ this.$store.state.username }}&nbsp;&nbsp;</span>
+            </div>
+            <el-dropdown-menu slot="dropdown">
+              <router-link :to="{ path: '/personcenter', query: { user: this.$store.state.username } }">
+                <el-dropdown-item command="a">个人中心</el-dropdown-item>
+              </router-link>
+              <el-dropdown-item command="b">退出登录</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
         </div>
-      </el-col>
-    </div>
+      </div>
+    </el-col>
+  </div>
 </template>
 
 <script>
@@ -68,7 +68,7 @@ export default {
 
   methods: {
     goto_login() {
-      this.$router.push({ path: '/login'});
+      this.$router.push({ path: '/login' });
     },
     goto_logout() {
       this.$confirm('此操作将退出登录, 是否继续?', '提示', {
@@ -95,8 +95,6 @@ export default {
 </script>
 
 <style scoped>
-
-
 #top {
   box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
   background-color: white;
@@ -104,4 +102,11 @@ export default {
   margin-bottom: 3px;
 }
 
+a {
+  text-decoration: none;
+}
+
+.router-link-active {
+  text-decoration: none;
+}
 </style>
--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -9,42 +9,60 @@
                     <i class="el-icon-collection"></i>
                     项目名
                 </template>
-                VueProj
+                {{proj_name}}
             </el-descriptions-item>
             <el-descriptions-item>
                 <template slot="label">
                     <i class="el-icon-user"></i>
                     项目创建人
                 </template>
-                李田所
+                {{proj_creator}}
             </el-descriptions-item>
             <el-descriptions-item>
                 <template slot="label">
-                    <i class="el-icon-s-promotion"></i>
+                    <i class="fa-solid fa-people-group"></i>
                     项目所属团队
                 </template>
-                啊对对队
+                {{proj_team}}
             </el-descriptions-item>
             <el-descriptions-item>
                 <template slot="label">
                     <i class="el-icon-s-custom"></i>
                     参与成员
                 </template>
-                德川,我修院,李田所,周撅伦,淳平
+                <span v-for="item in members1" :key="item.userid" @click="toPC">
+                <el-tooltip class="item" effect="light" :content="item.truename" placement="bottom">
+                    <el-avatar size="15" :src="item.photo"></el-avatar>
+                </el-tooltip>
+                </span>
+            </el-descriptions-item>
+            <el-descriptions-item>
+                <template slot="label">
+                    <i class="el-icon-date"></i>
+                    项目开始时间
+                </template>
+                {{proj_start}}
+            </el-descriptions-item>
+            <el-descriptions-item>
+                <template slot="label">
+                    <i class="el-icon-date"></i>
+                    项目结束时间
+                </template>
+                {{proj_end}}
             </el-descriptions-item>
             <el-descriptions-item>
                 <template slot="label">
                     <i class="el-icon-office-building"></i>
                     项目描述
                 </template>
-                兄啊，这么臭的项目有存在的必要吗(恼)
+                {{proj_info}}
             </el-descriptions-item>
         </el-descriptions>
         <br>
         <!--项目功能模块跳转-->
         <el-col :span="6">
-            <el-card :body-style="{ padding: '0px' }">
-                <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+            <el-card :body-style="{ padding: '5px' }" shadow="hover">
+                <img src="https://s2.loli.net/2022/05/06/f2Jx6BkcSLEnRtU.jpg"
                     class="image">
                 <div style="padding: 14px;">
                     <span>原型设计</span>
@@ -56,8 +74,8 @@
         </el-col>
 
         <el-col :span="6" :offset="3">
-            <el-card :body-style="{ padding: '0px' }">
-                <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+            <el-card :body-style="{ padding: '5px' }" shadow="hover">
+                <img src="https://s2.loli.net/2022/05/06/f2Jx6BkcSLEnRtU.jpg"
                     class="image">
                 <div style="padding: 14px;">
                     <span>绘制图</span>
@@ -69,8 +87,8 @@
         </el-col>
 
         <el-col :span="6" :offset="3">
-            <el-card :body-style="{ padding: '0px' }">
-                <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+            <el-card :body-style="{ padding: '5px' }" shadow="hover">
+                <img src="https://s2.loli.net/2022/05/06/f2Jx6BkcSLEnRtU.jpg"
                     class="image">
                 <div style="padding: 14px;">
                     <span>编辑文档</span>
@@ -84,24 +102,80 @@
 </template>
 
 <script>
+import qs from "qs";
 export default {
+    name: "ProjectView",
     data() {
         return {
-
+            proj_name: '', //项目名称
+            proj_creator: '', //项目创建者昵称
+            proj_start: '', //项目开始时间
+            proj_end: '', //项目结束时间
+            proj_team: '', //所属团队名称
+            proj_info: '', //项目简介
+            members: [], //用户列表
+            members1: [
+                {
+                    userid: 1,
+                    truename: "张三",
+                    photo: "https://s2.loli.net/2022/05/06/f2Jx6BkcSLEnRtU.jpg"
+                },{
+                    userid: 2,
+                    truename: "李四",
+                    photo: "https://s2.loli.net/2022/05/06/f2Jx6BkcSLEnRtU.jpg"
+                },{
+                    userid: 3,
+                    truename: "王五",
+                    photo: "https://s2.loli.net/2022/05/06/f2Jx6BkcSLEnRtU.jpg"
+                },{
+                    userid: 4,
+                    truename: "老六",
+                    photo: "https://s2.loli.net/2022/05/06/f2Jx6BkcSLEnRtU.jpg"
+                }
+            ], //测试用
+            proj_photo: '', //项目图片
         };
     },
     methods: {
-        toPD (){ //Prototype design
-            this.$router.push({ path: '/'});
+        toPD() { //Prototype design
+            this.$router.push({ path: '/' });
         },
-        toPE (){//Photo Edit
-            this.$router.push({ path: '/'});
+        toPE() {//Photo Edit
+            this.$router.push({ path: '/' });
         },
-        toFE (){//File edit
-            this.$router.push({ path: '/'});
+        toFE() {//File edit
+            this.$router.push({ path: '/' });
         },
+        toPC() {//Personal Center
+            this.$router.push({ path: '/personcenter' });
+        }
     },
-
+    mounted (){//获取项目相关信息
+            this.$axios.post('/project/detail', qs.stringify({ proj_id: this.$route.query.id }), {
+                headers: {
+                    userid: this.$store.state.userid,
+                    token: this.$store.state.token,
+                }
+            })
+                .then(res => {
+                    if (res.data.errno === 0) {
+                        this.proj_name = res.data.proj_name;
+                        this.proj_creator = res.data.proj_creator;
+                        this.proj_start = res.data.proj_start;
+                        this.proj_end = res.data.proj_end;
+                        this.proj_team = res.data.proj_team;
+                        this.proj_info = res.data.proj_info;
+                        this.members = res.data.members;
+                        this.proj_photo = res.data.proj_photo;
+                    }
+                    else {
+                        this.$message.error(res.data.msg);
+                    }
+                })
+                .catch(err => {
+                    this.$message.error(err);
+                });
+    },
 }
 </script>
 
