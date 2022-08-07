6dc2be812ea02c241a917d700497d561290a9a6f
iLinks <3043564076@qq.com>
2022-08-07 17:12:09 +0800

    verifycode register

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="is_success===0" class="register">
+  <div v-if="register_state===0" class="register">
     <h1>欢迎注册</h1>
     <el-input placeholder="输入用户名" v-model="username" clearable style="width:300px"></el-input>
     <p>
@@ -11,16 +11,26 @@
     <p>
     <el-input placeholder="输入邮箱" v-model="email" clearable style="width:300px"></el-input>
     <div>
-      <el-button @click="register" round>注册</el-button>
+      <el-button @click="send_verifycode" round>注册</el-button>
       <router-link to="/login" style="margin: 5px;">
         <el-button round>去登录</el-button>
       </router-link>
     </div>
   </div>
+  <div v-else-if="register_state===1">
+    <br><br>
+      <el-input placeholder="输入验证码" v-model="verifycode_input" clearable style="width:300px"></el-input>
+      <!-- <el-button @click="check_verifycode" round style="margin: 5px;">重发</el-button> -->
+      <div>
+        <br>
+      <el-button @click="check_verifycode" round style="margin: 5px;">确定</el-button>
+      <!-- <el-button @click="check_verifycode" round style="margin: 5px;">重发</el-button> -->
+    </div>
+  </div>
   <div v-else>
     <!-- <el-divider></el-divider> -->
     <br>
-    <h3 style="color:rgb(100,100,100)">注册成功，您的uid为{{uid_toshow}}</h3>
+    <h3 style="color:rgb(100,100,100)">{{this.username}}注册成功</h3>
     <!-- <el-divider></el-divider> -->
     <router-link to="/login" style="margin: 5px;">
         <el-button round>去登录</el-button>
@@ -39,11 +49,13 @@ import qs from "qs";
         password_2: "",
         email: "",
         uid_toshow:undefined,
-        is_success:0,
+        register_state:0, //0:输入信息界面 1:输入验证码界面 2:注册成功
+        verifycode:undefined,
+        verifycode_input:undefined,
       }
     },
     methods: {
-      register() {
+      send_verifycode() {
         if (this.username === '') {
           this.$message.error('用户名不能为空');
         }
@@ -59,14 +71,64 @@ import qs from "qs";
         else if (this.email === '') {
           this.$message.error('邮箱不能为空');
         }
+        else if (this.password_1 != this.password_2) {
+          this.$message.error('两次输入密码不一致');
+        }
         else {
-          this.$axios.post('/team/register', qs.stringify(this.$data))  
+          let sendVerifyCode_ifo = {
+            email:this.email,
+            judge:1,
+          }
+          console.log(sendVerifyCode_ifo.email+'\n'+sendVerifyCode_ifo.judge)
+          this.$axios.post('team/sendVerifyCode', qs.stringify(sendVerifyCode_ifo))
+  .then((res) => {
+    if (res.data.errno === 0) {
+      this.$message.success(res.data.msg);
+      this.verifycode = res.data.code;
+      this.register_state = 1;
+    } else {
+      // alert(123);
+      this.$message.error(res.data.msg);
+    }
+  })
+  .catch((err) => {
+    this.$message.error(err);
+  });
+        }
+      },
+      
+      send_again() {
+        let sendVerifyCode_ifo = {
+            email:this.email,
+            judge:1,
+          }
+          console.log(sendVerifyCode_ifo.email+'\n'+sendVerifyCode_ifo.judge)
+          this.$axios.post('projteam/sendVerifyCode', qs.stringify(sendVerifyCode_ifo))
+  .then((res) => {
+    if (res.data.errno === 0) {
+      this.$message.success(res.data.msg);
+      this.verifycode = res.data.code;
+      this.register_state = 1;
+    } else {
+      this.$message.error(res.data.msg);
+    }
+  })
+  .catch((err) => {
+    this.$message.error(err);
+  });
+      },
+
+      check_verifycode() {
+            if (this.verifycode != this.verifycode_input) {
+              this.$message.error('验证码错误');
+            }
+            else {
+                this.$axios.post('/team/register', qs.stringify(this.$data))  
             .then(res => {
               if (res.data.errno === 0) {
                 this.$message.success('注册成功');
-                // alert(res.data.userid);
                 this.uid_toshow = res.data.userid;
-                this.is_success = 1;
+                this.register_state = 2;
               }
               else {
                 this.$message.error(res.data.msg);
@@ -75,8 +137,9 @@ import qs from "qs";
             .catch(err => {
               this.$message.error(err);
             });
-        }
+            }
       }
+
     }
   }
 </script>
